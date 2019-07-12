19302aef8346c5b67afe42dd113f9efa6f5fb456
choang10 <choang10@gmail.com>
2019-07-12 09:08:20 -0500

    add marketing app

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Substitute env vars
-envsubst '${CLIENT_URL} ${SERVER_URL}' \
+envsubst '${CLIENT_URL} ${SERVER_URL} ${MARKETING_URL}' \
 < /etc/nginx/conf.d/frontend.conf.template \
 > /etc/nginx/conf.d/default.conf
 
